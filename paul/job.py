af4aa5f81f0f26db459b136e83f0bcc8d4714c21
Léo Cavaillé <leo@cavaille.net>
2013-08-15 18:12:03 +0200

    :x

---
+++ paul/job.py
@@ -0,0 +1,35 @@
+# coding=utf-8
+from clint.textui import puts, colored
+import cmd
+import shlex
+
+import paul.client
+
+import texttable as tt
+
+def wrong_syntax(cmd):
+    print "Wrong syntax, check syntax with \"help create\""
+
+def pretty_print_pause(b):
+    if b:
+        puts(colored.red("Lucy is PAUSED"))
+    else:
+        puts(colored.green("Lucy is working"))
+
+class Job(cmd.Cmd):
+    def do_paused(self, s):
+        """
+        Usage :
+            paused - returns the status of lucy instance
+            paused 0|1 - set the pause or resume
+        """
+        if s:
+            paul.client.proxy.set_paused_status(s)
+            pretty_print_pause(paul.client.proxy.get_paused_status(s))
+        else:
+            pretty_print_pause(paul.client.proxy.get_paused_status(s))
+
+    # Catch ^D
+    def do_EOF(self, line):
+        print ''
+        return True
--- paul/modules.py
+++ paul/modules.py
@@ -1,6 +1,7 @@
 import cmd
 from paul.user import User
 from paul.machine import Machine
+from paul.job import job
 
 class Modules(cmd.Cmd):
     prompt = 'paul > '
@@ -15,6 +16,12 @@ class Modules(cmd.Cmd):
         m = Machine()
         m.prompt = 'paul:machine > '
         m.cmdloop()
+
+    def do_job(self, s):
+        "Load jobs module to interact with the jobs on lucy instance"
+        j = Job()
+        j.prompt = 'paul:job > '
+        j.cmdloop()
 
     # Catch ^D
     def do_EOF(self, line):
